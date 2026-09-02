1944600e3b4493df8fafadf577ffd1c59498037e
Heig Gregorian <theheig@gmail.com>
2026-09-01 23:58:50 -0700

    Use movement counter as report sequence

--- src/app_dataformats.c
+++ src/app_dataformats.c
@@ -106,8 +106,10 @@ encode_to_5 (uint8_t * const output,
     ep_data.pressure_pa       = rd_sensor_data_parse (data, RD_SENSOR_PRES_FIELD);
     ep_data.temperature_c     = rd_sensor_data_parse (data, RD_SENSOR_TEMP_FIELD);
     ep_data.measurement_count = ep_5_measurement_count;
-    uint8_t mvtctr = (uint8_t) (app_sensor_event_count_get() % (RE_5_MVTCTR_MAX + 1));
-    ep_data.movement_count    = mvtctr;
+    // uint8_t mvtctr = (uint8_t) (app_sensor_event_count_get() % (RE_5_MVTCTR_MAX + 1));
+    // ep_data.movement_count    = mvtctr;
+    ep_data.movement_count =
+        (uint8_t) (ep_5_measurement_count % (RE_5_MVTCTR_MAX + 1));
     err_code |= ri_radio_address_get (&ep_data.address);
     err_code |= ri_adv_tx_power_get (&ep_data.tx_power);
     err_code |= rt_adc_vdd_get (&ep_data.battery_v);
